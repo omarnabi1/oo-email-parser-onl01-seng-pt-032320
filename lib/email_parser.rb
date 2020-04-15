ddfcae92f5ddb85db45c04d6dea31827b8f5b93f
Omar Nabi <onabi20@gmail.com>
2020-04-15 20:43:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,4 +7,14 @@ class EmailAddressParser
   
   attr_accessor :email 
   
-  def initialize 
+  def  initialize(emails)
+    @email = emails
+  end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
